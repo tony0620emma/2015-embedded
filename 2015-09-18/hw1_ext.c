e6126ac3d8df81f54b961853bbed6ebb6aa67f3d
tony0620emma <tony0620emma@yahoo.com.tw>
2015-09-30 14:34:05 +0800

    use FMA instruction to improve performance

--- 2015-09-18/hw1_ext.c
+++ 2015-09-18/hw1_ext.c
@@ -43,24 +43,53 @@ double compute_pi_avxsimd(size_t dt)
 	return pi * 4.0;
 }
 
+double compute_pi_avxsimd_v2(size_t dt)
+{
+
+	double pi = 0.0;
+	double delta = 1.0 / dt;
+	register __m256d ymm0, ymm1, ymm2, ymm3, ymm4;
+	ymm0 = _mm256_set1_pd(1.0);
+	ymm1 = _mm256_set1_pd(delta);
+	ymm2 = _mm256_set_pd(delta * 3, delta * 2, delta * 1, 0.0);
+	ymm4 = _mm256_setzero_pd();
+
+	for (int i = 0; i <= dt - 4; i += 4) {
+		ymm3 = _mm256_set1_pd(i * delta);
+		ymm3 = _mm256_add_pd(ymm3, ymm2);
+		ymm3 = _mm256_fmadd_pd(ymm3, ymm3, ymm0);
+		ymm3 = _mm256_div_pd(ymm1, ymm3);
+		ymm4 = _mm256_add_pd(ymm4, ymm3);
+	}
+	double tmp[4] __attribute__((aligned(32)));
+	_mm256_store_pd(tmp, ymm4);
+	pi += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+
+	return pi * 4.0;
+}
+
 int main(int argc, char *argv[])
 {
 	clock_gettime(CLOCK_MONOTONIC, &timespec);
 	double start_time = timespec.tv_sec + 
 	       		((double)(timespec.tv_nsec / 10000)) / 100000;	
 	size_t i;
-	for (i = 4; i < 100000; i++) {
+	for (i = 0; i < 3; i++) {
 #if defined(AVXSIMD)
-		compute_pi_avxsimd(i);
+		compute_pi_avxsimd(128000000);
+#elif defined(AVXSIMD2)
+		printf("pi = %lf\n", compute_pi_avxsimd_v2(128000000));
 #elif defined(BASE)
-		compute_pi_base(i);
+		compute_pi_base(128000000);
 #endif
 	}
 	clock_gettime(CLOCK_MONOTONIC, &timespec);
 	double end_time = timespec.tv_sec + 
 	       		((double)(timespec.tv_nsec / 10000)) / 100000;
 #if defined(AVXSIMD)
-		printf("\n\nAVXSIMD version\n");
+		printf("\n\nAVXSIMD version 1\n");
+#elif defined(AVXSIMD2)
+		printf("\n\nAVXSIMD version 2\n");
 #elif defined(BASE)
 		printf("\n\nBASE version\n");
 #endif
